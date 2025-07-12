df3ff269ae3159dc7454434d161d09bb5eb1f2ab
HusseinMahmoud98 <hussein.mahmoud.fouad98@gmail.com>
2025-07-12 17:01:49 +0300

    Q14

--- Assignment04/Program.cs
+++ Assignment04/Program.cs
@@ -383,6 +383,48 @@
 
             #endregion
 
+            #region Q14
+            /* 14- Consider an Array of Integer values with size N, having values as write a program to find the longest distance between Two equal cells.
+             * In this example. The distance is measured by the number Of cells- for example, the distance between the first and the
+             * fourth cell is 2 (cell 2 and cell 3).
+             * In the example above, the longest distance is between the first 7 and the 10th 7, with a distance of 8 cells, i.e. the number of cells between the 1st 
+             * And the 10th 7s. 
+             * Note:
+             * Array values will be taken from the user If you have input like 1111111 then the distance is the number of Cells between the first and the last cell. 
+             */
+            int N, longest_distance = 0, distance = 0;
+            Console.Write("Enter the number of cells: ");
+            N = int.Parse(Console.ReadLine());
+
+            int[] Cells = new int[N];
+
+
+            Console.WriteLine("Enter cells' values");
+            for (int i = 0; i < N; i++)
+            {
+                Cells[i] = int.Parse(Console.ReadLine());
+            }
+
+            //get the largest distance
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = i + 1; j < N; j++)
+                {
+                    if (Cells[i] == Cells[j])
+                    {
+                        distance = j - i - 1;
+
+                        if (distance > longest_distance)
+                        {
+                            longest_distance = distance;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine($"longest distance = {longest_distance}");
+            #endregion
+
 
         }
     }
